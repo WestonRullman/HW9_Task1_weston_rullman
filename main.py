9801398b82a14f807191a26cf672226ae8561757
Weston Rullman <>
2023-04-06 22:19:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# HW9_Task1_weston_rullman
+# File: Task1_weston_rullman.py
+# Date: 04 03 2023
+# By: Weston Rullman
+# ID: 234660
+# Team: 6
+#
+# ELECTRONIC SIGNATURE
+# Weston Rullman
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# The purpose of this script is to calculate the intrinsic impedence using the amplitude of the incident wave, the permittivity, and the permeability of mediums 1 and 2.
+
+# input the inputs
+E = float(input('Amplitude of the Incident Wave (V/m): '))
+e1 = float(input('Permittivity of Medium 1: '))
+e2 = float(input('Permittivity of Medium 2: '))
+mu1 = float(input('Permiability of Medium 1: '))
+mu2 = float(input('Permiability of Medium 2: '))
+
+# This calculates the intrinsic impedence of mediums 1(n1) and 2(n2)
+n1 = 
+n2 = 
+# Do the Math
+# This calculates the amplitude of the reflected wave(E1) and the transmitted wave(E2)
+E1 = (2*n)
+E2 = 
