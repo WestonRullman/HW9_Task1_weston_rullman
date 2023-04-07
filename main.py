9bc54668121ac838c650255aa8bd09b25d3e9ad7
Weston Rullman <78664774+WestonRullman@users.noreply.github.com>
2023-04-07 18:00:47 +0000

    Final Version. Tested for Correctness.

--- main.py
+++ main.py
@@ -23,9 +23,15 @@ mu1 = float(input('Permiability of Medium 1: '))
 mu2 = float(input('Permiability of Medium 2: '))
 
 # This calculates the intrinsic impedence of mediums 1(n1) and 2(n2)
-n1 = 
-n2 = 
+n1 = 377.14 * (mu1/e1)**.5
+n2 = 377.14 * (mu2/e2)**.5
 # Do the Math
 # This calculates the amplitude of the reflected wave(E1) and the transmitted wave(E2)
-E1 = (2*n)
-E2 = 
+Et = ((2 * n2)/(n2 + n1)) * E
+Er = ((n2 - n1)/(n2 + n1)) * E
+
+#Display Outputs
+print('Intrinsic Impedance 1: ' + str(n1) + ' Ohms')
+print('Intrinsic Impedance 2: ' + str(n2) + ' Ohms')
+print('Reflected Amplitude: ' + str((Er)) + ' V/m')
+print('Transmitted Amplitude: ' + str((Et)) + ' V/m')
